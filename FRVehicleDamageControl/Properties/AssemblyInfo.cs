240d670a18cd5ad9161ba3bc380d8292bc00f5d1
GitHub <sup.per2003@yandex.ru>
2019-06-30 22:54:18 +0300

    Fixs

--- FRVehicleDamageControl/Properties/AssemblyInfo.cs
+++ FRVehicleDamageControl/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("FRCarDamageUtils")]
+[assembly: AssemblyTitle("FRVehicleDamageControl")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FRCarDamageUtils")]
+[assembly: AssemblyProduct("FRVehicleDamageControl")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
